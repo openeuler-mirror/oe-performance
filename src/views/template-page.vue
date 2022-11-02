e616d066ddf1ef6f51bf01c4a81b0ede8f94b3a1
Oxide_xyn <xyn430430@hotmail.com>
2022-11-03 01:16:57 +0800

    feat: 路由嵌套支持

--- src/views/template-page.vue
+++ src/views/template-page.vue
@@ -8,6 +8,14 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  console.log(router)
+})
 </script>
 
 <style>
