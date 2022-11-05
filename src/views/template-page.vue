63d15301930b601a3ceac6db6686dd084980d53f
Oxide_xyn <xyn430430@hotmail.com>
2022-11-05 18:56:28 +0800

    feat: 增加mock数据到6条，增加version_feature字段作为版本特性

--- src/views/template-page.vue
+++ src/views/template-page.vue
@@ -3,7 +3,7 @@
     template1
   </div>
   <div class="oe-perf-section">
-    template2
+    <el-button @click="getDetailData">测试数据</el-button>
   </div>
 </template>
 
@@ -11,11 +11,19 @@
 import { onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 
+import { getDetail } from '@/api/detail'
+
 const router = useRouter()
 
 onMounted(() => {
   console.log(router)
 })
+
+const getDetailData = () => {
+  getDetail(10).then((res) => {
+    console.log(res.data.results)
+  })
+}
 </script>
 
 <style>
