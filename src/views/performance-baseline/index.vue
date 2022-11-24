e50c38f36652efca640ce91fe0a00a8efbfc8f3f
Oxide_xyn <xyn430430@hotmail.com>
2022-11-24 09:10:20 +0800

    feat: 测试search数据获取。可以拿到submit_id集合jobs集合

--- src/views/performance-baseline/index.vue
+++ src/views/performance-baseline/index.vue
@@ -13,6 +13,7 @@ import testCase from './components/test-case.vue';
 import TestSubassembly from './components/test-subassembly.vue';
 import { onMounted } from 'vue'
 
+import { getPerformanceData } from '@/api/performance'
 import { getDetail } from '@/api/detail'
 
 const Data = ref<any[]>([])
@@ -20,7 +21,55 @@ onMounted(() => {
   getDetail(10).then((res) => {
     Data.value = res.data.results
   })
+  // getSubmitId()
+  // 获取unixbench下的submitID list
+  getPerformanceData({
+    'index': 'jobs',
+    'query': {
+      'query': {
+        'term': {
+          'suite': 'unixbench'
+        }
+      },
+      'aggs': {
+        'jobs_terms': {
+          'terms': {
+            'field': 'submit_id',
+            'size': 10000  // 取全量
+          }
+        }
+      }
+    },
+  })
+  // 根据submitId获取它的jobs
+  getPerformanceData({
+    'index': 'jobs',
+    'query': {
+      'size': 10000,  // 取全量
+      'query': {
+        'term': {
+          'submit_id': '005c5966-1f49-4452-a239-30c5ac1b8d72'
+        }
+      }
+    },
+  })
 })
+
+const getSubmitId = () => {
+  // 华为的实例，获取job，只展示submit数据
+  getPerformanceData({
+    'index': 'jobs',
+    'query': {
+      'size': 10,
+      '_source': ['submit_id'],
+      'query': {
+        'term': {
+          'suite': 'unixbench'
+        }
+      }
+    }
+  })
+}
 </script>
 
 <style>
