bae0b49e5bf2b8402d805578614f6ab80bcbb292
Oxide_xyn <xyn430430@hotmail.com>
2023-01-31 00:17:23 +0800

    feat: 表格增加“测试人”列

--- src/views/performance-baseline/config-file.ts
+++ src/views/performance-baseline/config-file.ts
@@ -36,6 +36,12 @@ export const columnConfig = [
     width: 300
   },
   {
+    label: '测试人',
+    prop: 'my_account',
+    show: true,
+    width: 300
+  },
+  {
     label: '性能数据',
     prop: 'performance_data',
     show: true
@@ -44,228 +50,50 @@ export const columnConfig = [
 
 export const columnConfig2 = [
   {
-    label: '提交编号',
-    prop: 'sbumit_id',
-    show: true
-  },
-  {
-    label: '提交日期',
-    prop: 'submit_date',
-    show: true
-  },
-  {
-    label: '提交时间',
-    prop: 'submit_time',
-    show: true
-  },
-  {
-    label: '操作系统',
-    prop: 'os_release',  // 需要拼接
-    show: true
-  },
-  {
-    label: '测试机',
-    prop: 'testbox',
-    show: true
-  },
-  {
-    label: '系统架构',
-    prop: 'os_arch',
-    show: true
-  },
-  {
-    label: 'CPU核数',
-    prop: 'nr_cpu',
-    show: true
-  },
-  {
-    label: 'CPU内核',
-    prop: 'kernel_version',
-    show: true
-  },
-  {
-    label: '内存配置',
-    prop: 'memory',
-    show: true
-  },
-  {
-    label: '操作系统页表大小',
-    prop: 'nr_node',
-    show: true
-  },
-  {
-    label: '创建人',
-    prop: 'created_by',
-    show: true
-  },
-  {
-    label: '测试人',
-    prop: 'tested_by',
-    show: true
-  },
-  {
     label: '服务器型号',
-    prop: '',
-    show: true
+    prop: 'testbox',
+    show: true,
+    width: 260,
   },
   {
-    label: '数据状态',
-    prop: 'job_state',
-    show: true
+    label: 'CPU频率',
+    prop: 'device.cpu.cpu_max_mhz',
+    show: true,
+    width: 100
   },
   {
     label: 'CPU总核数',
-    prop: '',
-    show: true
+    prop: 'nr_cpu',
+    show: true,
+    width: 120
   },
   {
-    label: 'CPU频率MHz',
-    prop: '',
-    show: true
-  },
-  {
-    label: '操作系统页表大小',
-    prop: '',
-    show: true
-  },
-  {
-    label: '测试模型',
+    label: 'CPU型号',
     prop: 'model_name',
-    show: true
+    show: true,
+    width: 300
   },
   {
-    label: '命令行',
-    prop: '',
-    show: true
-  },
-  {
-    label: '单位(描述)',
-    prop: '',
-    show: true
-  },
-  {
-    label: '指标',
-    prop: '',
-    show: true
-  },
-  {
-    label: 'CPU总线程数',
-    prop: '',
-    show: true
-  },
-  {
-    label: '所属系列',
-    prop: '',
-    show: true
-  },
-  {
-    label: 'CPU配置',
-    prop: '',
-    show: true
-  },
-  {
-    label: '内存配置',
-    prop: '',
-    show: true
-  },
-  {
-    label: '硬盘配置',
-    prop: '',
-    show: true
-  },
-  {
-    label: '硬盘型号',
-    prop: '',
-    show: true
-  },
-  {
-    label: 'Raid卡配置',
-    prop: '',
-    show: true
-  },
-  {
-    label: 'Raid组',
-    prop: '',
-    show: true
-  },
-  {
-    label: '网卡配置',
-    prop: '',
-    show: true
-  },
-  {
-    label: 'PSU配置',
-    prop: '',
-    show: true
-  },
-  {
-    label: '风扇配置',
-    prop: '',
-    show: true
-  },
-  {
-    label: '板卡配置',
-    prop: '',
-    show: true
-  },
-  {
-    label: 'BMC版本',
-    prop: '',
-    show: true
-  },
-  {
-    label: 'BIOS版本',
-    prop: '',
-    show: true
-  },
-  {
-    label: 'BIOS配置详情',
-    prop: '',
-    show: true
+    label: '操作系统',
+    prop: 'osv',  // 需要拼接
+    show: true,
+    width: 260
   },
   {
     label: '系统内核',
-    prop: '',
-    show: true
+    prop: 'kernel_version',
+    show: true,
+    width: 300
   },
   {
-    label: '编译器版本',
-    prop: '',
-    show: true
-  },
-  {
-    label: '测试工具版本',
-    prop: '',
-    show: true
-  },
-  {
-    label: 'GLIBC版本',
-    prop: '',
-    show: true
-  },
-  {
-    label: 'JDK版本',
-    prop: '',
-    show: true
-  },
-  {
-    label: 'numa信息',
-    prop: '',
-    show: true
+    label: '测试人',
+    prop: 'my_account',
+    show: true,
+    width: 300
   },
   {
     label: '性能数据',
-    prop: 'perf-profile',
-    show: true
-  },
-  {
-    label: '测试模型',
-    prop: '',
-    show: true
-  },
-  {
-    label: '创建时间',
-    prop: '',
+    prop: 'performance_data',
     show: true
   }
 ]
